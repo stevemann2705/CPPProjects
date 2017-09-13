f72f74582e27da17d99ed9b244888595bf033e47
Steve Mann <stevemann2705@gmail.com>
2017-09-13 09:03:04 +0530

    Added: display()
    Added includes to iostream and the display function to display value of x

--- space.cpp
+++ space.cpp
@@ -2,6 +2,9 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+
+using namespace std;
 
 class space{
 	int x;
@@ -22,6 +25,10 @@ public:
 	void operator -(){
 		x=-x;
 	}
+	
+	void display(){
+		cout << "x=" << x << endl;
+	}
 
 	//friend int operator -(space &s);
 }
@@ -35,8 +42,10 @@ int _tmain(int argc, _TCHAR* argv[])
 	space s1;
 	space s2(4);
 	space s3(10);
+	s3.display();
 	-s3;
 	//s1 = -s2;
+	s3.display();
 	return 0;
 }
 
